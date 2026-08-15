f68ba4da9e5b3ddad9e63bf30ec3f2b8ea8c076a
mealet <jasonragonter@gmail.com>
2026-08-15 22:00:55 +0300

    init: initial setup with colorscheme

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,19 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Zeen',
+			logo: {
+				light: './src/assets/logo/Zeen Box Contrast.svg',
+				dark: './src/assets/logo/Zeen Box Contrast.svg',
+			},
+			customCss: [
+				'./src/styles/custom.css',
+			],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/mealet/zeen' }],
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
-						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', slug: 'guides/example' },
 					],
 				},
